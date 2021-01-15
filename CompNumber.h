f5935c9da0cf928dcabc9fedfef9c71ea4c56b2b
Albina <zagrevskaia.as@students.dvfu.ru>
2021-01-16 03:33:16 +0900

    second commit

--- CompNumber.h
+++ CompNumber.h
@@ -9,5 +9,3 @@ Complex_Number operator-(Complex_Number& number1, Complex_Number& number2);
 Complex_Number operator*(Complex_Number& number1, Complex_Number& number2);
 Complex_Number operator/(Complex_Number& number1, Complex_Number& number2);
 std::ostream& operator<<(std::ostream& out, Complex_Number& number);
-
-
